877a8a405eaece163444933658cf77c855b230e0
Luis Jasso <luisnoejasso@gmail.com>
2019-06-20 11:29:24 -0500

    add templates

--- microservices/email-sender/src/scrape.js
+++ microservices/email-sender/src/scrape.js
@@ -2,28 +2,26 @@
 /* eslint-disable import/first */
 import config from 'config'
 import SendGrid from '@sendgrid/mail'
-import QueryString from 'query-string'
 import templateBase from './templateBase'
-import { log } from '../../../utils'
+import log from '@matersoft/utils'
 
 export default async function scrape(event) {
-  log('hola mundo ')
-  log(config.get('sendGrid.apikey'))
   log(event)
-  // const params = QueryString.parse(event.pathParameters.params)
-  // const html = templateBase({ matricula: params.matricula, mensaje: params.mensaje })
-  // const apikey = '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'
-  // SendGrid.setApiKey(apikey)
-  // const email = `${params.matricula}@upemor.edu.mx`
+  const {
+    correo, asunto, template, mensaje,
+  } = event.pathParameters
+  const html = templateBase({ matricula: correo, mensaje, template })
+  SendGrid.setApiKey(config.get('sendGrid.apikey'))
+  const email = `${correo}@upemor.edu.mx`
 
-  // const msg = {
-  //   from: 'pambiental@upemor.edu.mx',
-  //   to: email,
-  //   subject: `CECAM ${params.asunto}`,
-  //   html,
-  // }
+  const msg = {
+    from: 'sgambiental@upemor.edu.mx',
+    to: email,
+    subject: `CECAM ${asunto}`,
+    html,
+  }
 
-  // const data = await SendGrid.sendMultiple(msg)
-  // return data
-  return Promise.resolve('resolve')
+  const data = await SendGrid.sendMultiple(msg)
+  return data
+  // return Promise.resolve('resolve')
 }
--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,10 +1,10 @@
 
 const log = console.log
 console.log = function () {
-  // Invoke the original method with an additional parameter
   log.apply(
     console,
     [(new Date().toString())].concat([].slice.call(arguments))
   )
 }
+
 export default log
