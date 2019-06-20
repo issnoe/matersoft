3bdd223501d72c897b9810d0813075b813f23efa
Luis Jasso <luisnoejasso@gmail.com>
2019-06-19 20:44:30 -0500

    refactor

--- microservices/email-sender/src/scrape.js
+++ microservices/email-sender/src/scrape.js
@@ -1,26 +1,29 @@
 /* eslint-disable no-console */
 /* eslint-disable import/first */
 import config from 'config'
-import Debug from 'debug'
 import SendGrid from '@sendgrid/mail'
 import QueryString from 'query-string'
 import templateBase from './templateBase'
+import { log } from '../../../utils'
 
-const debug = Debug('scrape:volcanoes')
 export default async function scrape(event) {
-  const params = QueryString.parse(event.pathParameters.params)
-  const html = templateBase({ matricula: params.matricula, mensaje: params.mensaje })
-  const apikey = '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'
-  SendGrid.setApiKey(apikey)
-  const email = `${params.matricula}@upemor.edu.mx`
+  log('hola mundo ')
+  log(config.get('sendGrid.apikey'))
+  log(event)
+  // const params = QueryString.parse(event.pathParameters.params)
+  // const html = templateBase({ matricula: params.matricula, mensaje: params.mensaje })
+  // const apikey = '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'
+  // SendGrid.setApiKey(apikey)
+  // const email = `${params.matricula}@upemor.edu.mx`
 
-  const msg = {
-    from: 'pambiental@upemor.edu.mx',
-    to: email,
-    subject: `CECAM ${params.asunto}`,
-    html,
-  }
+  // const msg = {
+  //   from: 'pambiental@upemor.edu.mx',
+  //   to: email,
+  //   subject: `CECAM ${params.asunto}`,
+  //   html,
+  // }
 
-  const data = await SendGrid.sendMultiple(msg)
-  return data
+  // const data = await SendGrid.sendMultiple(msg)
+  // return data
+  return Promise.resolve('resolve')
 }
